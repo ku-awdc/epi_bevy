57c06c5627d451e82d14e41f5e513059fc4b1e64
Mossa <cgmossa@gmail.com>
2021-04-30 13:20:03 +0200

    Comments..

--- src/between_herd_spread_model_record.rs
+++ src/between_herd_spread_model_record.rs
@@ -1,4 +1,13 @@
-use std::collections::{BTreeMap, BTreeSet};
+///
+/// The [record_total_infected_farms] is very ineffective, due to having to
+/// query every farm for its disease state, in order to determine if 
+/// these are new outbreaks, or just old ones that are progressing.
+/// 
+/// - [ ] However, from here we cannot have a system that records between-herd
+/// incidence outside of the between-herd spread model...
+/// 
+
+use std::collections::{BTreeMap};
 
 use bevy::prelude::*;
 use bevy::{ecs::system::SystemParam, utils::HashSet};
@@ -12,7 +21,11 @@ pub struct Recorder<'a> {
     previously_active_infections: Local<'a, HashSet<Entity>>,
     total_infected_farms: Local<'a, BTreeMap<ScenarioTick, usize>>,
 }
-pub fn record_total_infected_farms(mut recorder: Recorder, query: Query<(Entity, &Infected)>) {
+pub fn record_total_infected_farms(
+    mut recorder: Recorder,
+    scenario_tick: Res<ScenarioTick>,
+    query: Query<(Entity, &Infected)>,
+) {
     let actively_infected_farms: HashSet<_> = query
         .iter()
         .filter(|x| {
@@ -22,18 +35,21 @@ pub fn record_total_infected_farms(mut recorder: Recorder, query: Query<(Entity,
         .map(|x| x.0)
         .collect();
     let tmp = recorder.previously_active_infections.clone();
-    let mut newly_infected_farms =
-        (&actively_infected_farms).difference(&tmp);
+    let mut newly_infected_farms = (&actively_infected_farms).difference(&tmp);
 
-    //FIXME: update previously
-
-    recorder
-        .previously_active_infections
-        .extend(newly_infected_farms.clone());
+    //TODO: update previously infected farms in an "efficient" way.
+    // There must be a way to register these things and see
+    // if that is computationally better than doing this, maybe through
+    // "tag" like components.
+    *recorder.previously_active_infections = actively_infected_farms.clone();
 
     if newly_infected_farms.next().is_some() {
         // there are some newly infected farms.
+        let total_infected_farms = actively_infected_farms.len();
 
-        info!("Total infected farms: {}", &actively_infected_farms.len());
+        recorder
+            .total_infected_farms
+            .insert(*scenario_tick, total_infected_farms);
+        info!("Total infected farms: {}", total_infected_farms);
     }
 }

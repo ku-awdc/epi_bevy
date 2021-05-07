bd9015abcc131b25fac3ec463df79174a087c91b
Mossa <cgmossa@gmail.com>
2021-05-07 05:39:17 +0200

    Added a Query<Trait>-example. It doesn't work yet.

--- examples/query_traits.rs
+++ examples/query_traits.rs
@@ -2,7 +2,23 @@
 // TODO: This example showcases querying entities on trait implementations
 // in bevy. Is this possible?
 
+use epi_bevy::prelude::*;
+
 fn main() {
-    todo!()
+    let mut miniworld = World::new();
+    let entity_id = miniworld.spawn().insert(432).id();
+
+    // Query<Trait> doesn't work
+    // Query<dyn Trait> doesn't work
+    // Query<Box<Trait>> doesn't work
+    fn my_system(query: Query<Box<std::fmt::Debug>>) {
+        query.for_each(|x| {
+            dbg!(x);
+        });
+    }
+
+    let mut some_stage = SystemStage::single(my_system.system());
+
+    some_stage.run(miniworld);
+
 }
-//

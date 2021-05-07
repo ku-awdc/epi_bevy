bd4b6ee8c8de23d07b75551da5aeac4e501353e8
CGMossa <cgmossa@gmail.com>
2021-05-07 15:38:28 +0200

    Gotta implement repetitions and thus need to do some tinkering.

---
+++ examples/universe.rs
@@ -0,0 +1,12 @@
+//! Create a [Universe] struct that contains a list of [Worlds] that seemingly
+//! act as a sub-worlds.
+//!
+//! - [ ] Incorporate ideas in the Subworld RFC on the bevyengine/rfc repo.
+//! - [ ] Create CelestialResources that are shared among worlds.
+//! - [ ] Define the end the update of a Subworld and/or the
+//!
+//!
+
+use epi_bevy::prelude::*;
+
+fn main() {}

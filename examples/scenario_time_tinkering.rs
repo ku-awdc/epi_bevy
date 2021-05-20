e2a0a25d730c4c675386a26cf6d2b1160c555945
CGMossa <cgmossa@gmail.com>
2021-05-20 17:34:41 +0200

    Documentation

--- examples/scenario_time_tinkering.rs
+++ examples/scenario_time_tinkering.rs
@@ -1,3 +1,9 @@
+//
+// This example was made because there was an issue where the simulation wasn't
+// running at all, and this was to see if the simple scenario with a
+// ticker would run.
+// 
+// 
 use bevy::{
     app::{AppExit, ScheduleRunnerPlugin},
     diagnostic::{DiagnosticsPlugin, LogDiagnosticsPlugin},

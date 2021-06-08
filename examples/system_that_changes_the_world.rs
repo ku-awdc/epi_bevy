b562fe98142fa0e2eb4058d929e3d31dfad53f0d
Mossa <cgmossa@gmail.com>
2021-06-08 14:35:57 +0200

    Added an example where we change the world container directly

---
+++ examples/system_that_changes_the_world.rs
@@ -0,0 +1,51 @@
+//
+// Here we investigate if systems can wholeheartedly take hold of [World] and
+// ammend entities with some properties, circumventing the command-framework.
+//
+//
+//
+use epi_bevy::{populations::HerdSize, prelude::*};
+
+fn main() {
+    let mut world = World::new();
+    let mut rng = StdRng::seed_from_u64(20210608);
+
+    world.insert_resource(HerdSize::new_single_population(100));
+
+    let entities: Vec<Entity> = (0..14)
+        .map(|_| rng.gen_range(90..145))
+        .map(|number| world.spawn().insert_bundle(((), number)).id())
+        .collect_vec();
+    world.insert_resource(rng);
+    world.insert_resource(entities);
+
+    let mut main_loop = SystemStage::single_threaded();
+
+    main_loop.add_system(add_rate_to_everyone.exclusive_system());
+    main_loop.run(&mut world);
+
+    let entities = world.get_resource::<Vec<Entity>>().cloned().unwrap();
+    let random_flag = entities
+        .into_iter()
+        .filter_map(|x| world.get::<bool>(x))
+        .copied()
+        .collect_vec();
+    dbg!(random_flag);
+}
+
+/// This has to be an `.exclusive_system` and these can only have `&mut World`
+/// as argument. But locally we can inquiry the world about these things. 
+fn add_rate_to_everyone(world: &mut World) {
+    let entities = world.get_resource::<Vec<Entity>>().cloned().unwrap();
+    let mut rng = world.get_resource_mut::<StdRng>().unwrap();
+    entities
+        .clone()
+        .into_iter()
+        .map(|_| (rng.gen_bool(0.2),))
+        .collect_vec()
+        .into_iter()
+        .zip_eq(entities.into_iter())
+        .for_each(|(components, entity)| {
+            world.entity_mut(entity).insert_bundle(components);
+        });
+}

faf814289c8d917f822ffc09db220eee7eee7c85
Mossa <cgmossa@gmail.com>
2021-06-08 23:49:12 +0200

    `cargo fmt`

--- src/scenario_time/mod.rs
+++ src/scenario_time/mod.rs
@@ -1,7 +1,6 @@
 //! This module contains a timer object, bevy-systems for updating (using)
 //! the timer, and run-criterias that uses this timer.
-//! 
-
+//!
 
 pub mod scenario_intervals;
 pub mod scenario_timer;
